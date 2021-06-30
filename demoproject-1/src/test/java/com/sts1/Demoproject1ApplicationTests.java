2c29b32a09edc51ea8d039d8170555b4ea264b61
gajanan157 <86602529+gajanan157@users.noreply.github.com>
2021-06-30 11:54:17 +0530

    Add files via upload

---
+++ demoproject-1/src/test/java/com/sts1/Demoproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sts1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
